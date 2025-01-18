465a40ad616203bbc6ac66a5d7470c2ae857239d
Sachi <aliapudang@gmail.com>
2025-01-18 21:06:05 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,28 @@
+class restaurantMenu:
+    def _init_(self):
+      self.meu_items = {}
+
+    def add_item(self, name, price):
+      self.menu_items[name] = price
+
+    def get_price(self, name):
+      return self.menu_items.get(name, None)
+
+def main():
+  menu = RestaurantMenu()
+  # add initiral menu items
+  menu.add_item("gurger", 10.99)
+  menu add_item("fries", 4.99)
+
+if_name_== "_main_":
+  main()
+
+
+
+
+
+
+
+
+
 
